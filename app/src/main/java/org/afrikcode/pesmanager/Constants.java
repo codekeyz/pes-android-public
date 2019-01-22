43d0d71b11f984b1fbb192a971f065cd4abd92ed
Hover Software Solutions <ositachima.co@gmail.com>
2019-01-22 10:31:22 +0000

    Refactored Database Paths

--- app/src/main/java/org/afrikcode/pesmanager/Constants.java
+++ app/src/main/java/org/afrikcode/pesmanager/Constants.java
@@ -2,19 +2,19 @@ package org.afrikcode.pesmanager;
 
 public class Constants {
 
-    public static final String ManagersRef = "managers";
+    public static final String ManagersRef = "Managers";
 
-    public static final String ClientsRef = "clients";
+    public static final String ClientsRef = "Clients";
 
-    public static final String TransactionsRef = "transactions";
+    public static final String TransactionsRef = "Transactions";
 
-    public static final String YearTimelineRef = "years";
-    public static final String MonthTimelineRef = "months";
-    public static final String WeekTimelineRef = "weeks";
-    public static final String DayTimelineRef = "days";
+    public static final String YearTimelineRef = "Years";
+    public static final String MonthTimelineRef = "Months";
+    public static final String WeekTimelineRef = "Weeks";
+    public static final String DayTimelineRef = "Days";
+    public static final String serviceRef = "Services";
 
     public static final String BRANCH_ID = "Branch";
     public static final String BRANCH_NAME = "BranchName";
     public static final String MANAGER_ID = "Manager";
-    public static final String serviceRef = "services";
 }
